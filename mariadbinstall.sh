e86c3b2748d1d5517633d37a32d5911429130b0b
willam-gregoire <willam-gregoire@users.noreply.github.com>
2015-12-16 19:05:39 +0100

    Update mariadbinstall.sh

--- mariadbinstall.sh
+++ mariadbinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-sudo debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password password PASS'
-sudo debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password_again password PASS'
+echo mariadb-server-10.1 mysql-server/root_password password PASS | debconf-set-selections
+echo mariadb-server-10.1 mysql-server/root_password_again password PASS | debconf-set-selections
+
+
 sudo apt-get update
 sudo apt-get install -y mariadb-server
 mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('')"
