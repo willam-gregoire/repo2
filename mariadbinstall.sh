7df2555af30bb6c391118d3287fe81da6342cb2f
willam-gregoire <willam-gregoire@users.noreply.github.com>
2015-12-16 19:54:24 +0100

    Update mariadbinstall.sh

--- mariadbinstall.sh
+++ mariadbinstall.sh
@@ -2,22 +2,26 @@
 
 apt-get -y update
 
-logger "Installing updates"
+logger "Installing WordPress"
 
 # Set up a silent install of MySQL
+dbpass=$1
+
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-sudo apt-get install -y mysql-server
 
-logger "Installing MySQL"
+# Install the LAMP stack and WordPress
+apt-get -y install apache2 mysql-server php5 php5-mysql wordpress
 
+# Setup WordPress
+gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
+bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
 
+ln -s /usr/share/wordpress /var/www/html/wordpress
+mv /etc/wordpress/config-localhost.php /etc/wordpress/config-default.php
 
-# export DEBIAN_FRONTEND=noninteractive
-# echo mariadb-server-10.1 mysql-server/root_password password PASS | debconf-set-selections
-# echo mariadb-server-10.1 mysql-server/root_password_again password PASS | debconf-set-selections
-# sudo apt-get update -y
-# sudo apt-get install -y mariadb-server
-# mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('')"
+# Restart Apache
+apachectl restart
 
+logger "Done installing WordPress; open /wordpress to configure"
